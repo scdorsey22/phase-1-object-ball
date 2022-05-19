3d6bb9e470b3064d0af65e07a539dd11736d2d88
Shane Dorsey <scdorsey22@gmail.com>
2022-05-19 15:08:42 -0700

    Need to create function

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,19 +1,59 @@
 
-const gameObject = new Object() {
+const gameObject = {
     home: {
         teamName: "Brooklyn Nets",
         colors: ["Black", "White"],
         players: {
-            "": {
-                "number": 
-                "shoe": 
-                "points": 
-                "rebounds": 
-                "assists": 
-                "steals": 
-                "blocks": 
-                "slamDunks": 
-            }
+            "Alan Anderson": {
+                number: "0",
+                shoe: "16",
+                points: "22",
+                rebounds: "12",
+                assists: "12",
+                steals: "3",
+                blocks: "1",
+                slamDunks: "1",
+            },
+            "Reggie Evans": {
+                number: "30",
+                shoe: "14",
+                points: "12",
+                rebounds: "12",
+                assists: "12",
+                steals: "12",
+                blocks: "12",
+                slamDunks: "7",
+            },
+            "Brook Lopez": {
+                number: "11",
+                shoe: "17",
+                points: "17",
+                rebounds: "19",
+                assists: "10",
+                steals: "3",
+                blocks: "1",
+                slamDunks: "15",
+            },    
+            "Mason Plumlee": {
+                number: "0",
+                shoe: "16",
+                points: "22",
+                rebounds: "12",
+                assists: "12",
+                steals: "3",
+                blocks: "1",
+                slamDunks: "1",
+            },    
+            "Alan Anderson": {
+                number: "0",
+                shoe: "16",
+                points: "22",
+                rebounds: "12",
+                assists: "12",
+                steals: "3",
+                blocks: "1",
+                slamDunks: "1",
+            },        
         },
     },
     away: {
@@ -22,3 +62,5 @@ const gameObject = new Object() {
         players: {},
     }
 }
+
+console.log(gameObject())
