30c35268bbadbe3930a4250dfcded017fea6fa01
Shane Dorsey <scdorsey22@gmail.com>
2022-05-19 14:49:52 -0700

    First code

---
+++ src/00-objectball.js
@@ -0,0 +1,24 @@
+
+const gameObject = new Object() {
+    home: {
+        teamName: "Brooklyn Nets",
+        colors: ["Black", "White"],
+        players: {
+            "": {
+                "number": 
+                "shoe": 
+                "points": 
+                "rebounds": 
+                "assists": 
+                "steals": 
+                "blocks": 
+                "slamDunks": 
+            }
+        },
+    },
+    away: {
+        teamName: "Charlotte Hornets",
+        colors: ["Turquoise", "Purple"],
+        players: {},
+    }
+}
